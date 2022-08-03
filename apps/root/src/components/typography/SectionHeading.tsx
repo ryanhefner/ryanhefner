05f360477ac893e6bbf421dd156ddb306a0d22e0
Ryan Hefner <hi@ryanhefner.com>
2022-08-03 11:49:43 -0400

    Ad typography components

---
+++ apps/root/src/components/typography/SectionHeading.tsx
@@ -0,0 +1,12 @@
+import { Heading } from '@chakra-ui/react'
+
+export const SectionHeading = ({ children, ...rest }) => (
+  <Heading
+    as="h3"
+    fontSize={{ base: '4xl', lg: '5xl' }}
+    fontWeight="medium"
+    {...rest}
+  >
+    {children}
+  </Heading>
+)
