13c34990abe732d0d438e1c091e3062227c12f8b
Ryan Hefner <hi@ryanhefner.com>
2022-08-03 11:48:10 -0400

    Update ClientGridItem responsive styles

--- apps/root/src/components/clients/ClientGridItem.tsx
+++ apps/root/src/components/clients/ClientGridItem.tsx
@@ -3,12 +3,12 @@ import { Center, Image } from '@chakra-ui/react'
 export const ClientGridItem = ({ name, imageUrl }) => (
   <Center
     flexBasis={{ base: '50%', md: '33.3%', lg: '20%' }}
-    minH={{ base: 120, md: 180, lg: 180 }}
+    minH={{ base: 120, md: 180 }}
   >
     <Image
       alt={name}
       src={imageUrl}
-      w={{ base: '75%', lg: '50%' }}
+      w={{ base: '75%', md: '50%' }}
     />
   </Center>
 )
