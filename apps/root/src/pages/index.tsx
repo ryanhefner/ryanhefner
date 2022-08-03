fbf04d600ce499f90de400a4cddcd98bdf2bbbcb
Ryan Hefner <hi@ryanhefner.com>
2022-08-02 22:54:53 -0400

    Update home page to use projects data

--- apps/root/src/pages/index.tsx
+++ apps/root/src/pages/index.tsx
@@ -2,39 +2,42 @@ import { Box, Flex, Heading, Text } from '@chakra-ui/react'
 import { Link } from 'components/base'
 import { ProjectLink } from 'components/projects'
 import { PageWrapper } from 'components/site'
+import { getProjectsByUrls } from 'data/projects'
 
-const IndexPage = () => (
-  <PageWrapper>
-    <Heading as="h1" fontSize={{ base: '6xl', md: '9xl' }} fontWeight="medium" lineHeight="none">
-      Ryan Hefner<br />
-      <Text color="gray.600">
-        Software Engineer + Eternal Tinkerer
-      </Text>
-    </Heading>
-    <Flex flexDir={{ base: 'column', md: 'row' }} w="full" mt={16}>
-      <Box flexBasis={{ base: '100%', md: '50%' }} mb={16}>
-        <Link href="/projects">
-          <Heading as="h2" fontSize={{ base: '3xl', md: '5xl' }} fontWeight="medium">
-            Active Projects
-          </Heading>
-        </Link>
-        <Box my={8}>
-          <ProjectLink
-            name="Pregraph"
-            description="GraphQL API analytics, caching, and federation tools."
-            slug="/projects/pregraph"
-          />
+const IndexPage = () => {
+  const activeProjects = getProjectsByUrls(['https://www.pregraph.com'])
+
+  return (
+    <PageWrapper>
+      <Heading as="h1" fontSize={{ base: '6xl', md: '9xl' }} fontWeight="medium" lineHeight="none">
+        Ryan Hefner<br />
+        <Text color="gray.600">
+          Software Engineer + Eternal Tinkerer
+        </Text>
+      </Heading>
+      <Flex flexDir={{ base: 'column', md: 'row' }} w="full" mt={16}>
+        <Box flexBasis={{ base: '100%', md: '50%' }} mb={16}>
+          <Link href="/projects">
+            <Heading as="h2" fontSize={{ base: '3xl', md: '5xl' }} fontWeight="medium">
+              Active Projects
+            </Heading>
+          </Link>
+          <Box my={8}>
+            {activeProjects.map((project) => (
+              <ProjectLink key={project.url} {...project} />
+            ))}
+          </Box>
         </Box>
-      </Box>
-      {/* <Box flexBasis={{ base: '100%', md: '50%' }} mb={16}>
-        <Link href="/thoughts">
-          <Heading as="h2" fontSize={{ base: '3xl', md: '5xl' }} fontWeight="medium">
-            Recent Thoughts
-          </Heading>
-        </Link>
-      </Box> */}
-    </Flex>
-  </PageWrapper>
-)
+        {/* <Box flexBasis={{ base: '100%', md: '50%' }} mb={16}>
+          <Link href="/thoughts">
+            <Heading as="h2" fontSize={{ base: '3xl', md: '5xl' }} fontWeight="medium">
+              Recent Thoughts
+            </Heading>
+          </Link>
+        </Box> */}
+      </Flex>
+    </PageWrapper>
+  )
+}
 
 export default IndexPage
