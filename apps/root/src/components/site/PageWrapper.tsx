8d2e4e23096353a432ddccbe3d2e75ebc800b628
Ryan Hefner <hi@ryanhefner.com>
2022-08-04 13:30:08 -0400

    Update PageWrapper formatting and support for rest

--- apps/root/src/components/site/PageWrapper.tsx
+++ apps/root/src/components/site/PageWrapper.tsx
@@ -1,7 +1,12 @@
 import { Box } from '@chakra-ui/react'
 
-export const PageWrapper = ({ children }) => (
-  <Box flex="1" py={{ base: 6 }} px={{ base: 6, sm: 10, md: 16, xl: 24 }}>
+export const PageWrapper = ({ children, ...rest }) => (
+  <Box
+    flex="1"
+    py={{ base: 6 }}
+    px={{ base: 6, sm: 10, md: 16, xl: 24 }}
+    {...rest}
+  >
     {children}
   </Box>
 )
