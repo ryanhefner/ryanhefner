4991dee15962a8e8f001748616fa103754d49bc1
Ryan Hefner <hi@ryanhefner.com>
2022-11-10 10:52:40 -0500

    Update Link to use legacyBehavior

--- apps/root/src/components/base/Link.tsx
+++ apps/root/src/components/base/Link.tsx
@@ -4,7 +4,7 @@ import { chakra, Link as ChakraLink } from '@chakra-ui/react'
 const ChakraNextLink = chakra(NextLink)
 
 export const Link = ({ children, href, ...rest }) => (
-  <ChakraNextLink href={href} passHref>
+  <ChakraNextLink href={href} passHref legacyBehavior>
     <ChakraLink {...rest}>
       {children}
     </ChakraLink>
