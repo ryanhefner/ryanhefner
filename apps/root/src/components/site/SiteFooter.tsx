1690b5ec3d56f0511ce83e57c6e267d55e63d5f1
Ryan Hefner <hi@ryanhefner.com>
2023-03-14 17:52:07 -0400

    Add titles to SiteFooter links

--- apps/root/src/components/site/SiteFooter.tsx
+++ apps/root/src/components/site/SiteFooter.tsx
@@ -25,6 +25,7 @@ export const SiteFooter = () => {
           href="https://www.github.com/ryanhefner"
           target="_blank"
           rel="noopener noreferrer"
+          title="@ryanhefner on GitHub"
         >
           <Text as="span" fontFamily="mono" fontSize="sm">GitHub</Text>
         </Link>
@@ -32,6 +33,7 @@ export const SiteFooter = () => {
           href="https://www.twitter.com/ryanhefner"
           target="_blank"
           rel="noopener noreferrer"
+          title="@ryanhefner on Twitter"
         >
           <Text as="span" fontFamily="mono" fontSize="sm">Twitter</Text>
         </Link>
