b8c6e92a21c02d705a52777b79560a665796cafb
Ryan Hefner <hi@ryanhefner.com>
2023-03-14 21:55:09 -0400

    Update ProjectGridItem breakpoint sizes

--- apps/site/src/components/projects/ProjectGridItem.tsx
+++ apps/site/src/components/projects/ProjectGridItem.tsx
@@ -2,7 +2,13 @@ import { Box } from '@chakra-ui/react'
 
 export const ProjectGridItem = ({ children }) => (
   <Box
-    flexBasis={{ base: '100%', sm: '50%', md: '33.3%', lg: '25%', xl: '20%' }}
+    flexBasis={{
+      base: '100%',
+      md: '50%',
+      lg: '33.3%',
+      xl: '25%',
+      '2xl': '20%',
+    }}
     pr={{ base: 0, md: 5, lg: 10 }}
     pb={16}
   >
