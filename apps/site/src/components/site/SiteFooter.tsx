52dc0437e197d0813ae3977a03c9559a4432f06a
Ryan Hefner <hi@ryanhefner.com>
2023-03-16 17:42:16 -0400

    Add withOSS link to footer

--- apps/site/src/components/site/SiteFooter.tsx
+++ apps/site/src/components/site/SiteFooter.tsx
@@ -1,4 +1,4 @@
-import { Flex, HStack, Text, useColorModeValue } from '@chakra-ui/react'
+import { Flex, HStack, Image, Text, useColorModeValue } from '@chakra-ui/react'
 import { Link } from '../base'
 
 export const SiteFooter = () => {
@@ -16,11 +16,19 @@ export const SiteFooter = () => {
       py={{ base: 6 }}
       px={{ base: 6, sm: 10, md: 16, xl: 24 }}
     >
-      <Text fontFamily="mono" fontSize="sm">
-        Ryan Hefner &copy;{' '}
-        <Text as="span">2004 - {`${new Date().getFullYear()}`}. </Text>
-        May all your days be rad 🤙.
-      </Text>
+      <Flex
+        gap={{ base: 3, md: 6 }}
+        flexDir={{ base: 'column', md: 'row-reverse' }}
+      >
+        <Link href="/withoss">
+          <Image alt="With OSS" src="/assets/oss.svg" h={6} />
+        </Link>
+        <Text fontFamily="mono" fontSize="sm">
+          Ryan Hefner &copy;{' '}
+          <Text as="span">2004 - {`${new Date().getFullYear()}`}. </Text>
+          May all your days be rad 🤙.
+        </Text>
+      </Flex>
       <HStack spacing={{ base: 3, md: 6 }} mb={{ base: 5, md: 0 }}>
         <Link
           href="https://www.github.com/ryanhefner"
