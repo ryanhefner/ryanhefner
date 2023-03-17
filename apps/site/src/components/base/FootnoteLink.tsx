426b53b412363b6c4ca310f5263ead80d5b17561
Ryan Hefner <hi@ryanhefner.com>
2023-03-17 14:21:03 -0400

    Create FootnoteLink component

---
+++ apps/site/src/components/base/FootnoteLink.tsx
@@ -0,0 +1,15 @@
+import { chakra } from '@chakra-ui/react'
+import { Link } from '.'
+
+const Sup = chakra('sup', {
+  fontSize: '50%',
+  top: '-12px',
+})
+
+const FootnoteLink = ({ children, href, ...rest }) => (
+  <Link href={href} color="blue.500" {...rest}>
+    <Sup>{children}</Sup>
+  </Link>
+)
+
+export default FootnoteLink
