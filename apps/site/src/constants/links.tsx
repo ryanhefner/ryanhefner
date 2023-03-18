1a4b8191b75647fb1be0b60e4e845bdf7f4b4053
Ryan Hefner <hi@ryanhefner.com>
2023-03-17 23:55:23 -0400

    Add links constants

---
+++ apps/site/src/constants/links.tsx
@@ -0,0 +1,31 @@
+export const ExternalLink = {
+  FATHOM_CLIENT: 'https://github.com/derrickreimer/fathom-client',
+  FATHOM_REFERRAL: 'https://usefathom.com/ref/EKONBS',
+  JON_BLACK: 'https://jon.black',
+}
+
+export const InternalLink = {
+  ABOUT: '/about',
+  HOME: '/',
+  NOW: '/now',
+  PROJECTS: '/projects',
+  PROJECTS_OSS: '/projects/oss',
+  THOUGHTS: '/thoughts',
+  WITH_OSS: '/withoss',
+}
+
+export const ProjectLink = {
+  BETA_CODE: 'https://betacodes.io',
+  CASTERS: 'https://casters.io',
+  COLOR_SETS: 'https://colorsets.com',
+  COMMUNAL_WORKS: 'https://communal.works',
+  JAM_SESSIONS: 'https://jamsessions.org',
+  LINK_CARDS: 'https://linkcards.io',
+  OPTIMIZE_TOOLSET: 'https://optimizetoolset.com',
+  PKG_STATS: 'https://pkgstats.com',
+  PLAY_NICE: 'https://playnice.studio',
+  PREGRAPH: 'https://pregraph.com',
+  TICK_TOCK_TRACK: 'https://ticktocktrack.com',
+}
+
+export const OSSLink = {}
