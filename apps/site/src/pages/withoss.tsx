436eccccbea923640aabb47e7b249698bbf4d138
Ryan Hefner <hi@ryanhefner.com>
2023-11-30 00:03:45 -0500

    Fix asset url

--- apps/site/src/pages/withoss.tsx
+++ apps/site/src/pages/withoss.tsx
@@ -1,5 +1,12 @@
 import React, { Fragment } from 'react'
-import { Box, Flex, Heading, Image, Text, useColorModeValue } from '@chakra-ui/react'
+import {
+  Box,
+  Flex,
+  Heading,
+  Image,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react'
 import { SiteMeta } from 'next-meta'
 import { Link } from '../components/base'
 import { PageWrapper } from '../components/site'
@@ -27,7 +34,7 @@ const OssPage = () => {
           w/
           <Image
             display="inline-block"
-            src="/oss.svg"
+            src="/assets/oss.svg"
             alt="Open-Source Software"
             filter="invert(1)"
             h={{ base: 24, md: 32 }}
