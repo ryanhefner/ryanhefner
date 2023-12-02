202cd82d49a30177b3384c4c5571b2a8bbf91380
Ryan Hefner <hi@ryanhefner.com>
2023-12-02 10:08:15 -0500

    Comment out shadergradient stuff from about page

--- apps/site/src/pages/about.tsx
+++ apps/site/src/pages/about.tsx
@@ -1,9 +1,9 @@
 import { Box, Heading, Text } from '@chakra-ui/react'
 import { SiteMeta } from 'next-meta'
-import { ShaderGradientCanvas, ShaderGradient } from 'shadergradient'
-import * as reactSpring from '@react-spring/three'
-import * as drei from '@react-three/drei'
-import * as fiber from '@react-three/fiber'
+// import { ShaderGradientCanvas, ShaderGradient } from 'shadergradient'
+// import * as reactSpring from '@react-spring/three'
+// import * as drei from '@react-three/drei'
+// import * as fiber from '@react-three/fiber'
 import { Link } from '../components/base'
 import { ClientGrid, ClientGridItem } from '../components/clients'
 import { SiteLayout } from '../components/layouts'
@@ -27,18 +27,18 @@ const AboutPage = () => (
         top="45px"
         left={0}
       >
-        <ShaderGradientCanvas
+        {/* <ShaderGradientCanvas
           importedFiber={{ ...fiber, ...drei, ...reactSpring }}
           style={{
             position: 'absolute',
             top: 0,
           }}
         >
-          {/* <ShaderGradient
+          <ShaderGradient
             control="query"
             urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=1.2&cAzimuthAngle=180&cDistance=3.6&cPolarAngle=90&cameraZoom=1&color1=%2352ff89&color2=%23dbba95&color3=%23d0bce1&embedMode=off&envPreset=city&fov=45&gizmoHelper=hide&grain=on&lightType=3d&pixelDensity=1&positionX=-1.4&positionY=0&positionZ=0&reflection=0.1&rotationX=0&rotationY=10&rotationZ=0&shader=defaults&type=plane&uDensity=1.3&uFrequency=5.5&uSpeed=0.4&uStrength=4&uTime=0&wireframe=false&zoomOut=false"
-          /> */}
-        </ShaderGradientCanvas>
+          />
+        </ShaderGradientCanvas> */}
       </Box>
       <Heading
         as="h1"
