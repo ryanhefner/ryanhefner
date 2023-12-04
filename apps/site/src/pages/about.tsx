b2b6bea264269872e5564079e00268831bd8a9b8
Ryan Hefner <hi@ryanhefner.com>
2023-12-03 23:40:44 -0500

    Remove test accent

--- apps/site/src/pages/about.tsx
+++ apps/site/src/pages/about.tsx
@@ -18,29 +18,7 @@ const DESCRIPTION =
 const AboutPage = () => (
   <>
     <SiteMeta title={TITLE} description={DESCRIPTION} />
-    <PageWrapper position="relative">
-      <Box
-        display="block"
-        width="20px"
-        height="140px"
-        bg="blue.500"
-        position="absolute"
-        top="45px"
-        left={0}
-      >
-        {/* <ShaderGradientCanvas
-          importedFiber={{ ...fiber, ...drei, ...reactSpring }}
-          style={{
-            position: 'absolute',
-            top: 0,
-          }}
-        >
-          <ShaderGradient
-            control="query"
-            urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=1.2&cAzimuthAngle=180&cDistance=3.6&cPolarAngle=90&cameraZoom=1&color1=%2352ff89&color2=%23dbba95&color3=%23d0bce1&embedMode=off&envPreset=city&fov=45&gizmoHelper=hide&grain=on&lightType=3d&pixelDensity=1&positionX=-1.4&positionY=0&positionZ=0&reflection=0.1&rotationX=0&rotationY=10&rotationZ=0&shader=defaults&type=plane&uDensity=1.3&uFrequency=5.5&uSpeed=0.4&uStrength=4&uTime=0&wireframe=false&zoomOut=false"
-          />
-        </ShaderGradientCanvas> */}
-      </Box>
+    <PageWrapper>
       <PageHeading>About</PageHeading>
       <Box mt={16}>
         <Box maxW="container.md">
