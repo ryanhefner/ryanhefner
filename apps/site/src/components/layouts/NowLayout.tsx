aacf2e563bf964608c228d1e1ec33b3b6ea06295
Ryan Hefner <hi@ryanhefner.com>
2023-12-28 15:06:04 -0500

    Update NowLayout to render links based on contentlayer data

--- apps/site/src/components/layouts/NowLayout.tsx
+++ apps/site/src/components/layouts/NowLayout.tsx
@@ -1,9 +1,11 @@
 import { Box, Heading, HStack, Text, useColorModeValue } from '@chakra-ui/react'
 import { usePathname } from 'next/navigation'
+import { allNows } from 'contentlayer/generated'
+import { format } from 'date-fns'
 import { Link } from '../base'
 import { PageWrapper } from '../site'
 import { theme } from '../../styles'
-import { nowPages } from '../../data/now'
+// import { nowPages } from '../../data/now'
 import { SiteLayout } from './SiteLayout'
 
 export const NowLayout = ({ children }) => {
@@ -21,7 +23,42 @@ export const NowLayout = ({ children }) => {
               Now Archive
             </Heading>
           </Box>
-          {nowPages.map((page, index) => (
+          {allNows
+            .sort((a, b) => {
+              if (a.date > b.date) return -1
+              if (a.date < b.date) return 1
+              return 0
+            })
+            .map((now, index) => (
+              <Link
+                key={now.date}
+                href={`/now/${format(now.date, 'yyyy-MM-dd')}`}
+              >
+                <HStack borderBottom={`1px solid ${borderColor}`} py={2}>
+                  <Box
+                    borderRadius="full"
+                    boxSize={2}
+                    border={`1px solid ${borderColor}`}
+                    bgColor={
+                      (index === 0 && pathname === '/now') ||
+                      pathname.split('/').pop() ===
+                        format(now.date, 'yyyy-MM-dd')
+                        ? selectedBgColor
+                        : 'transparent'
+                    }
+                    ml={2}
+                  />
+                  <Text as="span" fontFamily="mono" fontSize="sm">{`${format(
+                    now.date,
+                    'yyyy-MM-dd',
+                  )} - `}</Text>
+                  <Text as="span" fontWeight="medium">
+                    {now.title}
+                  </Text>
+                </HStack>
+              </Link>
+            ))}
+          {/* {nowPages.map((page, index) => (
             <Link key={page.date} href={`/now/${page.date}`}>
               <HStack borderBottom={`1px solid ${borderColor}`} py={2}>
                 <Box
@@ -46,7 +83,7 @@ export const NowLayout = ({ children }) => {
                 </Text>
               </HStack>
             </Link>
-          ))}
+          ))} */}
         </Box>
         <Box mt={{ base: 10, md: 10 }} mb={{ base: 10, md: 24 }}>
           <Text>
