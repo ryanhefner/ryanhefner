b7a2ab933031da70ce0c953795fb7bab70991620
Ryan Hefner <hi@ryanhefner.com>
2024-02-23 13:39:29 -0500

    Add content page support to og-image and pre-render all og-image pages

--- apps/site/src/pages/og-image/[[...slug]].tsx
+++ apps/site/src/pages/og-image/[[...slug]].tsx
@@ -1,55 +1,17 @@
-import { useRouter } from 'next/router'
 import { Flex, Heading, Text } from '@chakra-ui/react'
+import { allNows } from 'contentlayer/generated'
+import { allThoughts } from 'contentlayer/generated'
+import { allUpdates } from 'contentlayer/generated'
+
 import { theme } from '../../styles'
 
-const SharePage = () => {
-  const router = useRouter()
+type SharePageProps = {
+  body: string
+  path: string
+  title: string
+}
 
-  const { slug } = router.query
-
-  const path = Array.isArray(slug) ? slug.join('/') : slug
-
-  let title
-  let body
-
-  switch (path) {
-    case 'about':
-      title = 'About'
-      body =
-        'I’m a software engineer and product designer living in Atlanta, GA.'
-      break
-
-    case 'withoss':
-      title = 'Made w/ OSS'
-      body = 'The open-source software I use to build this site.'
-      break
-
-    case 'projects':
-      title = 'Projects'
-      body = 'Projects I’ve worked on over the years.'
-      break
-
-    case 'projects/oss':
-      title = 'Projects / Open Source Software'
-      body = 'My open-source software contributions.'
-      break
-
-    case 'thoughts':
-      title = 'Thoughts'
-      body = 'A collection of thoughts, ideas, and notes.'
-      break
-
-    case 'now':
-      title = 'Now'
-      body = 'What I’m focusing on right now.'
-      break
-
-    default:
-      title = ''
-      body = 'Software Engineer + Eternal Tinkerer'
-      break
-  }
-
+const SharePage = ({ body, path, title }: SharePageProps) => {
   const borderColor = theme.colors.gray[600]
   return (
     <Flex
@@ -77,16 +39,146 @@ const SharePage = () => {
         fontFamily="mono"
         justify="space-between"
         borderTop={`1px solid ${borderColor}`}
-        fontSize="2xl"
+        fontSize="xl"
         pt={12}
       >
         <Text>@ryanhefner</Text>
-        <Text>{`ryanhefner.com${
-          !path || path === '/' ? '' : `/${path}`
-        }`}</Text>
+        <Text>{`ryanhefner.com${!path || path === '/' ? '' : `${path}`}`}</Text>
       </Flex>
     </Flex>
   )
+}
+
+export const getStaticPaths = async () => {
+  const staticPaths = [
+    {
+      params: {
+        slug: [''],
+      },
+    },
+    {
+      params: {
+        slug: ['/'],
+      },
+    },
+    {
+      params: {
+        slug: ['about'],
+      },
+    },
+    {
+      params: {
+        slug: ['now'],
+      },
+    },
+    {
+      params: {
+        slug: ['projects'],
+      },
+    },
+    {
+      params: {
+        slug: ['projects', 'oss'],
+      },
+    },
+    {
+      params: {
+        slug: ['thoughts'],
+      },
+    },
+    {
+      params: {
+        slug: ['withoss'],
+      },
+    },
+  ]
+
+  const nowPaths = allNows.map((now) => ({
+    params: {
+      slug: ['now', now.date],
+    },
+  }))
+
+  const thoughtPaths = allThoughts.map((thought) => ({
+    params: {
+      slug: ['thoughts', thought.slug],
+    },
+  }))
+
+  const updatePaths = allUpdates.map((update) => ({
+    params: {
+      slug: ['updates', update.slug],
+    },
+  }))
+
+  return {
+    paths: [...staticPaths, ...nowPaths, ...thoughtPaths, ...updatePaths],
+    fallback: true,
+  }
+}
+
+export const getStaticProps = async ({ params }) => {
+  const { slug } = params
+  const path = `/${Array.isArray(slug) ? slug.join('/') : slug ?? ''}`
+
+  let title = ''
+  let body = ''
+
+  if (path.startsWith('/now')) {
+    const now = allNows.find((now) => now.date === slug[1])
+    title = 'Now'
+    body = now?.title ?? ''
+  } else if (path.startsWith('/thoughts')) {
+    const thought = allThoughts.find((thought) => thought.slug === slug[1])
+    title = 'Thoughts'
+    body = thought?.title ?? ''
+  } else {
+    switch (path) {
+      case 'about':
+        title = 'About'
+        body =
+          'I’m a software engineer and product designer living in Atlanta, GA.'
+        break
+
+      case 'withoss':
+        title = 'Made w/ OSS'
+        body = 'The open-source software I use to build this site.'
+        break
+
+      case 'projects':
+        title = 'Projects'
+        body = 'Projects I’ve worked on over the years.'
+        break
+
+      case 'projects/oss':
+        title = 'Projects / Open Source Software'
+        body = 'My open-source software contributions.'
+        break
+
+      case 'thoughts':
+        title = 'Thoughts'
+        body = 'A collection of thoughts, ideas, and notes.'
+        break
+
+      case 'now':
+        title = 'Now'
+        body = 'What I’m focusing on right now.'
+        break
+
+      default:
+        title = ''
+        body = 'Software Engineer + Eternal Tinkerer'
+        break
+    }
+  }
+
+  return {
+    props: {
+      title,
+      body,
+      path,
+    },
+  }
 }
 
 export default SharePage
