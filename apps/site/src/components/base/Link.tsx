b3983384f878d99994b2cdb891ff7de3e83d1bb9
Ryan Hefner <hi@ryanhefner.com>
2024-04-10 23:53:11 -0400

    Add types to Link

--- apps/site/src/components/base/Link.tsx
+++ apps/site/src/components/base/Link.tsx
@@ -1,7 +1,12 @@
-import NextLink from 'next/link'
-import { Link as ChakraLink } from '@chakra-ui/react'
+import { PropsWithChildren } from 'react'
 
-export const Link = ({ children, href, ...rest }) => (
+import { Link as ChakraLink } from '@chakra-ui/react'
+import type { LinkProps as ChakraLinkProps } from '@chakra-ui/react'
+import NextLink from 'next/link'
+
+type LinkProps = PropsWithChildren & ChakraLinkProps
+
+export const Link = ({ children, href, ...rest }: LinkProps) => (
   <ChakraLink as={NextLink} href={href} {...rest}>
     {children}
   </ChakraLink>
