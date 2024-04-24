05abbeac8d73a09a9b4e019fd5f5c6793f2e4dc9
Ryan Hefner <hi@ryanhefner.com>
2024-04-24 00:34:30 -0400

    Update AudioPlayer to support playback and scrubbing, along with size support

--- apps/allplay/src/components/media/AudioPlayer.tsx
+++ apps/allplay/src/components/media/AudioPlayer.tsx
@@ -1,26 +1,41 @@
-import { useCallback, useContext, useEffect, useRef, useState } from 'react'
+import {
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react'
 
 import { Box, Flex, HStack, Text } from '@chakra-ui/react'
+import { Waveform } from '@waveforms/react'
 import Timecode from 'react-timecode'
 
 import { Link } from '../base'
-import { PlayButton } from '../buttons'
-
-import { Waveform } from './Waveform'
+import { PlayButton, PlayButtonSize } from '../buttons'
 import { PodcastPlayerContext } from '../../contexts'
 
+export enum AudioPlayerSize {
+  SMALL = 'SMALL',
+  LARGE = 'LARGE',
+}
+
 type AudioPlayerProps = {
+  disableLink?: boolean
   duration: number
   isSelected?: boolean
+  size?: AudioPlayerSize
   slug: string
-  title: string
+  title?: string
   url: string
   onPlay?: () => void
 }
 
 export const AudioPlayer = ({
+  disableLink,
   duration,
   isSelected,
+  size,
   slug,
   title,
   url,
@@ -28,15 +43,20 @@ export const AudioPlayer = ({
 }: AudioPlayerProps) => {
   const [waveformWidth, setWaveformWidth] = useState(0)
   const [waveformHeight, setWaveformHeight] = useState(0)
+  const [isDragging, setIsDragging] = useState(false)
+  const [dragPercent, setDragPercent] = useState(0)
 
-  const linkRef = useRef<HTMLDivElement>(null)
+  const dragRef = useRef<HTMLDivElement>(null)
 
-  const { currentTime, isPlaying } = useContext(PodcastPlayerContext)
+  const { seek } = useContext(PodcastPlayerContext)
+
+  const { currentTime, getAudioBuffer, isPlaying } =
+    useContext(PodcastPlayerContext)
 
   useEffect(() => {
     const handleResize = () => {
-      setWaveformWidth(linkRef.current?.clientWidth ?? 0)
-      setWaveformHeight(linkRef.current?.clientHeight ?? 0)
+      setWaveformWidth(dragRef.current?.clientWidth ?? 0)
+      setWaveformHeight(dragRef.current?.clientHeight ?? 0)
     }
 
     window.addEventListener('resize', handleResize)
@@ -50,6 +70,65 @@ export const AudioPlayer = ({
   const handlePlayClick = useCallback(() => {
     onPlay?.()
   }, [onPlay])
+
+  const audioBuffer = useMemo(() => getAudioBuffer(), [isPlaying, url])
+
+  const handleMouseDown = useCallback(
+    (evt: React.MouseEvent<HTMLDivElement>) => {
+      setIsDragging(true)
+    },
+    [],
+  )
+
+  useEffect(() => {
+    const handleMouseMove = (evt: MouseEvent | Event) => {
+      const event = evt as MouseEvent
+
+      if (dragRef.current) {
+        let percent = 0
+
+        const { left: dragRefLeft, right: dragRefRight } =
+          dragRef.current.getBoundingClientRect()
+
+        if (event.clientX > dragRefLeft && event.clientX < dragRefRight) {
+          percent = Math.max(
+            0,
+            Math.min(
+              1,
+              (event.clientX - dragRefLeft) / dragRef.current.clientWidth,
+            ),
+          )
+        } else if (event.clientX > dragRefRight) {
+          percent = 1
+        }
+
+        setDragPercent(percent)
+        seek(url, duration * 1000 * percent)
+      }
+    }
+
+    const handleMouseUp = (evt: Event | MouseEvent) => {
+      setIsDragging(false)
+      window.removeEventListener('mousemove', handleMouseMove)
+      window.removeEventListener('mouseup', handleMouseUp)
+    }
+
+    if (isDragging) {
+      window.addEventListener('mousemove', handleMouseMove)
+      window.addEventListener('mouseup', handleMouseUp)
+    } else {
+      window.removeEventListener('mousemove', handleMouseMove)
+      window.removeEventListener('mouseup', handleMouseUp)
+    }
+  }, [isDragging])
+
+  const scrubberOffset = useMemo(
+    () =>
+      isDragging
+        ? `calc(${dragPercent * 100}% - 3px)`
+        : `calc(${Math.min(1, Math.max(0, currentTime / (duration * 1000))) * 100}% - 3px)`,
+    [currentTime, dragPercent, duration, isDragging],
+  )
 
   return (
     <Flex
@@ -67,66 +146,87 @@ export const AudioPlayer = ({
       <PlayButton
         flex="0 0 auto"
         onClick={handlePlayClick}
+        isActive={isSelected}
         isPlaying={isSelected && isPlaying}
         m={2}
         mr={0}
+        size={
+          size === AudioPlayerSize.LARGE
+            ? PlayButtonSize.LARGE
+            : PlayButtonSize.SMALL
+        }
       />
-      <Link
-        href={`/episodes/${slug}`}
-        flex={1}
-        pl={2}
-        pos="relative"
-        h={10}
-        _hover={{
-          textDecoration: 'none',
-        }}
-      >
-        {isSelected ? (
-          <Waveform
-            color={isSelected ? '#ddd' : '#333'}
-            slug={slug}
-            zIndex={0}
-            height={`${waveformHeight}px`}
-            width={`${waveformWidth}px`}
-          />
-        ) : null}
-        <Flex
-          alignItems="center"
-          justifyContent="space-between"
-          zIndex={1}
-          pos="absolute"
-          top={0}
-          right={0}
-          bottom={0}
-          left={2}
-          overflow="clip"
-          ref={linkRef}
+      <Flex ref={dragRef} flex={1} pos="relative" ml={2}>
+        <Link
+          href={`/episodes/${slug}`}
+          flex={1}
+          h={10}
+          _hover={{
+            textDecoration: 'none',
+          }}
+          pointerEvents={disableLink ? 'none' : 'initial'}
+          userSelect={isDragging ? 'none' : 'initial'}
         >
-          <Text
-            fontSize="md"
+          {isSelected && audioBuffer ? (
+            <Waveform
+              audioBuffer={audioBuffer}
+              color={isSelected ? '#ddd' : '#333'}
+              zIndex={0}
+              height={waveformHeight}
+              width={waveformWidth}
+            />
+          ) : null}
+          <Flex
+            alignItems="center"
+            justifyContent="space-between"
+            zIndex={1}
+            pos="absolute"
+            top={0}
+            right={0}
+            bottom={0}
+            left={2}
             overflow="clip"
-            textOverflow="ellipsis"
-            whiteSpace="nowrap"
-            w={isSelected ? 'calc(100% - 114px)' : 'calc(100% - 50px)'}
           >
-            {title}
-          </Text>
-          <HStack spacing={1} mr={5}>
-            {isSelected ? (
-              <>
-                <Text fontFamily="monospace" fontSize="md">
-                  <Timecode format="mm:ss" time={currentTime} />
-                </Text>
-                <Text fontFamily="monospace" fontSize="md">
-                  /
-                </Text>
-              </>
-            ) : null}
-            <Text fontFamily="monospace" fontSize="md">
-              <Timecode format="mm:ss" time={duration * 1000} />
+            <Text
+              fontSize="md"
+              overflow="clip"
+              textOverflow="ellipsis"
+              whiteSpace="nowrap"
+              w={isSelected ? 'calc(100% - 114px)' : 'calc(100% - 50px)'}
+            >
+              {title ?? ''}
             </Text>
-          </HStack>
-        </Flex>
+            <HStack spacing={1} mr={5}>
+              {isSelected ? (
+                <>
+                  <Text
+                    fontFamily="monospace"
+                    fontSize={size === AudioPlayerSize.LARGE ? 'xl' : 'md'}
+                  >
+                    <Timecode
+                      format="mm:ss"
+                      time={
+                        isDragging ? duration * 1000 * dragPercent : currentTime
+                      }
+                    />
+                  </Text>
+                  <Text
+                    fontFamily="monospace"
+                    fontSize={size === AudioPlayerSize.LARGE ? 'xl' : 'md'}
+                  >
+                    /
+                  </Text>
+                </>
+              ) : null}
+              <Text
+                fontFamily="monospace"
+                fontSize={size === AudioPlayerSize.LARGE ? 'xl' : 'md'}
+              >
+                <Timecode format="mm:ss" time={duration * 1000} />
+              </Text>
+            </HStack>
+          </Flex>
+        </Link>
         {isSelected ? (
           <Box
             bg="red.500"
@@ -134,14 +234,36 @@ export const AudioPlayer = ({
             boxShadow="lg"
             pos="absolute"
             top="-5px"
-            left={`${(currentTime / (duration * 1000)) * 100}%`}
             h="calc(100% + 10px)"
             w="3px"
             cursor="grab"
             zIndex={2}
+            onMouseDown={handleMouseDown}
+            pointerEvents="all"
+            style={{
+              left: scrubberOffset,
+            }}
+            _before={{
+              content: '" "',
+              display: 'block',
+              w: 3,
+              h: 'full',
+              pos: 'absolute',
+              top: 0,
+              left: -3,
+            }}
+            _after={{
+              content: '" "',
+              display: 'block',
+              w: 3,
+              h: 'full',
+              pos: 'absolute',
+              top: 0,
+              left: '3px',
+            }}
           />
         ) : null}
-      </Link>
+      </Flex>
     </Flex>
   )
 }
