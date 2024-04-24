df33741e30b14e79094f60a42382eb801522f54a
Ryan Hefner <hi@ryanhefner.com>
2024-04-24 00:33:38 -0400

    Update PlayButton to support size prop

--- apps/allplay/src/components/buttons/PlayButton.tsx
+++ apps/allplay/src/components/buttons/PlayButton.tsx
@@ -2,18 +2,32 @@ import { Button, Icon } from '@chakra-ui/react'
 import type { ButtonProps } from '@chakra-ui/react'
 import { IoPauseSharp, IoPlaySharp } from 'react-icons/io5'
 
+export enum PlayButtonSize {
+  SMALL = 'SMALL',
+  LARGE = 'LARGE',
+}
 interface PlayButtonProps extends ButtonProps {
+  isActive?: boolean
   isPlaying?: boolean
+  size?: PlayButtonSize
 }
 
-export const PlayButton = ({ isPlaying, ...rest }: PlayButtonProps) => {
+export const PlayButton = ({
+  isActive,
+  isPlaying,
+  size = PlayButtonSize.SMALL,
+  ...rest
+}: PlayButtonProps) => {
   return (
     <Button
       border="1px solid"
-      borderColor="gray.500"
+      borderColor={isActive ? 'black' : 'gray.500'}
       borderRadius="full"
-      w={8}
-      h={8}
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      w={size === PlayButtonSize.LARGE ? 12 : 8}
+      h={size === PlayButtonSize.LARGE ? 12 : 8}
       p={0}
       minW={0}
       pos="relative"
@@ -22,9 +36,10 @@ export const PlayButton = ({ isPlaying, ...rest }: PlayButtonProps) => {
     >
       <Icon
         as={isPlaying ? IoPauseSharp : IoPlaySharp}
-        color={isPlaying ? 'black' : 'gray.500'}
-        pos="absolute"
-        left={isPlaying ? '7px' : 2}
+        color={isActive ? 'black' : 'gray.500'}
+        ml={isPlaying ? '0' : '3px'}
+        w={size === PlayButtonSize.LARGE ? 6 : 4}
+        h={size === PlayButtonSize.LARGE ? 6 : 4}
       />
     </Button>
   )
