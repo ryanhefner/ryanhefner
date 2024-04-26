0b5f129f8f0e32025634b5aef9af4e603f0542a3
Ryan Hefner <hi@ryanhefner.com>
2024-04-26 12:15:57 -0400

    Update Link to support smooth scrolling to hash links

--- apps/allplay/src/components/base/Link.tsx
+++ apps/allplay/src/components/base/Link.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from 'react'
+import { MouseEvent, PropsWithChildren, useCallback } from 'react'
 
 import { Link as ChakraLink } from '@chakra-ui/react'
 import type { LinkProps as ChakraLinkProps } from '@chakra-ui/react'
@@ -6,8 +6,26 @@ import NextLink from 'next/link'
 
 type LinkProps = PropsWithChildren & ChakraLinkProps
 
-export const Link = ({ children, href, ...rest }: LinkProps) => (
-  <ChakraLink as={NextLink} href={href} {...rest}>
-    {children}
-  </ChakraLink>
-)
+export const Link = ({ children, href, ...rest }: LinkProps) => {
+  const handleClick = useCallback(
+    (evt: MouseEvent) => {
+      if (href?.startsWith('#')) {
+        evt.preventDefault()
+
+        const target = document.querySelector(href)
+        window.scrollTo({
+          top: target?.getBoundingClientRect().top,
+          left: 0,
+          behavior: 'smooth',
+        })
+      }
+    },
+    [href],
+  )
+
+  return (
+    <ChakraLink as={NextLink} href={href} onClick={handleClick} {...rest}>
+      {children}
+    </ChakraLink>
+  )
+}
