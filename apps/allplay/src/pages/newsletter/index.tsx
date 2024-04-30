4f33dfbfd84c7b3e09a515ab32782a4ccb249664
Ryan Hefner <hi@ryanhefner.com>
2024-04-29 22:48:30 -0400

    Add signup form to newsletter page

--- apps/allplay/src/pages/newsletter/index.tsx
+++ apps/allplay/src/pages/newsletter/index.tsx
@@ -1,13 +1,27 @@
 import { ReactNode } from 'react'
 
-import { Flex, Heading } from '@chakra-ui/react'
+import { Box, Flex, Heading, Text } from '@chakra-ui/react'
+import { NewsletterForm } from 'newsletter'
 
 import { SiteLayout } from '../../components/layouts'
 
 const NewslettersIndexPage = () => {
   return (
-    <Flex>
-      <Heading>Newsletters</Heading>
+    <Flex
+      flexDir="column"
+      flex={1}
+      h="full"
+      px={{ base: 4, md: 8 }}
+      py={{ base: 12, md: 24 }}
+    >
+      <Box id="signup">
+        <Heading as="h3">Subscribe to the newletter</Heading>
+        <Text color="gray.400">
+          Get updates when new episodes are posted, and other fun stuff that I
+          am into.
+        </Text>
+        <NewsletterForm />
+      </Box>
     </Flex>
   )
 }
