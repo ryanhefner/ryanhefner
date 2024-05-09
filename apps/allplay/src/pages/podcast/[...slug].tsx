45f8db6d2ba659d1834d340a96debe8682d6524c
Ryan Hefner <hi@ryanhefner.com>
2024-05-08 21:51:30 -0400

    Add SiteMeta to podcast episode page

--- apps/allplay/src/pages/podcast/[...slug].tsx
+++ apps/allplay/src/pages/podcast/[...slug].tsx
@@ -15,6 +15,7 @@ import { AudioPlayer, AudioPlayerSize } from '../../components/media'
 import { PodcastPlayerContext } from '../../contexts'
 import { mdxComponents } from '../../mdx-components'
 import { EpisodeList } from '../../components/media/EpisodeList'
+import { SiteMeta } from 'next-meta'
 
 const SHOW_ID = process.env.NEXT_PUBLIC_TRANSISTOR_SHOW_ID
 
@@ -30,109 +31,118 @@ const EpisodePage = ({
   }, [episode, setCurrentEpisode])
 
   return (
-    <Flex
-      flexDir="column"
-      gap={{ base: 8, md: 12 }}
-      px={{ base: 4, md: 8 }}
-      py={{ base: 6, md: 12 }}
-    >
-      <Box>
-        <Heading
-          as="h1"
-          fontSize={{ base: '3xl', md: '7xl' }}
-          letterSpacing={{ base: -1, md: -2 }}
-        >
-          {episode?.attributes?.title ?? 'No episode title'}
-        </Heading>
-      </Box>
-      <Box>
-        <AudioPlayer
-          duration={episode.attributes.duration}
-          isSelected={currentEpisode?.id === episode.id}
-          size={AudioPlayerSize.LARGE}
-          slug={episode.attributes.slug}
-          url={episode.attributes.media_url}
-          onPlay={handlePlay}
-        />
-      </Box>
-      <Flex flexDir={{ base: 'column', md: 'row' }} gap={{ base: 24, md: 32 }}>
-        <Box
-          flex="0 0 30%"
-          alignSelf="flex-start"
-          pos={{ base: 'relative', md: 'sticky' }}
-          top={{ base: 0, md: 24 }}
-        >
-          <Heading as="h2" fontSize="lg">
-            Show Notes
-          </Heading>
-          <Flex flexDir="column" mt={{ base: 3, md: 4 }} gap={6}>
-            <Markdown
-              components={mdxComponents({
-                codeBg: 'gray.800',
-                codeColor: 'white',
-              })}
-              remarkPlugins={[remarkGfm as any]}
-            >
-              {episode.attributes.description_markdown}
-            </Markdown>
-          </Flex>
-        </Box>
-        <Box flex={1}>
+    <>
+      <SiteMeta
+        title={`Episode: ${episode?.attributes?.title ?? 'N/A'} - Podcast`}
+        description={episode?.attributes?.description ?? ''}
+      />
+      <Flex
+        flexDir="column"
+        gap={{ base: 8, md: 12 }}
+        px={{ base: 4, md: 8 }}
+        py={{ base: 6, md: 12 }}
+      >
+        <Box>
           <Heading
-            as="h2"
-            fontSize="lg"
-            pos="sticky"
-            top={{ base: 16, md: 24 }}
+            as="h1"
+            fontSize={{ base: '3xl', md: '7xl' }}
+            letterSpacing={{ base: -1, md: -2 }}
           >
-            Transcript
+            {episode?.attributes?.title ?? 'No episode title'}
           </Heading>
-          <Flex flexDir="column" mt={{ base: 3, md: 4 }} gap={6}>
-            {transcript?.segments?.map((segment: any, index: number) => (
-              <Flex key={`segment-${index}`} gap={8}>
-                <Box>
-                  {/* <Text color="gray.400">{segment.speaker}</Text> */}
-                  <Text
-                    color="gray.400"
-                    fontFamily="monospace"
-                    fontSize="sm"
-                    mt="3px"
-                    pos="sticky"
-                    top={{ base: 24, md: 32 }}
-                    whiteSpace="nowrap"
-                  >
-                    <Timecode
-                      format="mm:ss"
-                      time={parseInt(segment.startTime, 10) * 1000}
-                    />{' '}
-                    -{' '}
-                    <Timecode
-                      format="mm:ss"
-                      time={parseInt(segment.endTime, 10) * 1000}
-                    />
+        </Box>
+        <Box>
+          <AudioPlayer
+            duration={episode.attributes.duration}
+            isSelected={currentEpisode?.id === episode.id}
+            size={AudioPlayerSize.LARGE}
+            slug={episode.attributes.slug}
+            url={episode.attributes.media_url}
+            onPlay={handlePlay}
+          />
+        </Box>
+        <Flex
+          flexDir={{ base: 'column', md: 'row' }}
+          gap={{ base: 24, md: 32 }}
+        >
+          <Box
+            flex="0 0 30%"
+            alignSelf="flex-start"
+            pos={{ base: 'relative', md: 'sticky' }}
+            top={{ base: 0, md: 24 }}
+          >
+            <Heading as="h2" fontSize="lg">
+              Show Notes
+            </Heading>
+            <Flex flexDir="column" mt={{ base: 3, md: 4 }} gap={6}>
+              <Markdown
+                components={mdxComponents({
+                  codeBg: 'gray.800',
+                  codeColor: 'white',
+                })}
+                remarkPlugins={[remarkGfm as any]}
+              >
+                {episode.attributes.description_markdown}
+              </Markdown>
+            </Flex>
+          </Box>
+          <Box flex={1}>
+            <Heading
+              as="h2"
+              fontSize="lg"
+              pos="sticky"
+              top={{ base: 16, md: 24 }}
+            >
+              Transcript
+            </Heading>
+            <Flex flexDir="column" mt={{ base: 3, md: 4 }} gap={6}>
+              {transcript?.segments?.map((segment: any, index: number) => (
+                <Flex key={`segment-${index}`} gap={8}>
+                  <Box>
+                    {/* <Text color="gray.400">{segment.speaker}</Text> */}
+                    <Text
+                      color="gray.400"
+                      fontFamily="monospace"
+                      fontSize="sm"
+                      mt="3px"
+                      pos="sticky"
+                      top={{ base: 24, md: 32 }}
+                      whiteSpace="nowrap"
+                    >
+                      <Timecode
+                        format="mm:ss"
+                        time={parseInt(segment.startTime, 10) * 1000}
+                      />{' '}
+                      -{' '}
+                      <Timecode
+                        format="mm:ss"
+                        time={parseInt(segment.endTime, 10) * 1000}
+                      />
+                    </Text>
+                  </Box>
+                  <Text color="gray.400" fontSize="md">
+                    {segment.body}
                   </Text>
-                </Box>
-                <Text color="gray.400" fontSize="md">
-                  {segment.body}
-                </Text>
-              </Flex>
-            ))}
-          </Flex>
+                </Flex>
+              ))}
+            </Flex>
+          </Box>
+        </Flex>
+        <EpisodeList
+          episodes={episodes.filter((item: any) => item.id !== episode.id)}
+          mt={24}
+          title="More Episodes"
+        />
+        <Box id="#signup" mt={24}>
+          <Heading as="h3">Subscribe to the newletter</Heading>
+          <Text color="gray.400">
+            Get updates when new episodes are posted, and other fun stuff that I
+            am into.
+          </Text>
+          <NewsletterForm />
         </Box>
       </Flex>
-      <EpisodeList
-        episodes={episodes.filter((item: any) => item.id !== episode.id)}
-        mt={24}
-        title="More Episodes"
-      />
-      <Box id="#signup" mt={24}>
-        <Heading as="h3">Subscribe to the newletter</Heading>
-        <Text color="gray.400">
-          Get updates when new episodes are posted, and other fun stuff that I
-          am into.
-        </Text>
-        <NewsletterForm />
-      </Box>
-    </Flex>
+    </>
   )
 }
 
