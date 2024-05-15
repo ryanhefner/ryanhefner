69d32ea6389d8554787235b875de604220542c85
Ryan Hefner <hi@ryanhefner.com>
2024-05-15 11:33:24 -0400

    Process feed content via remark

--- apps/site/src/pages/feeds.tsx
+++ apps/site/src/pages/feeds.tsx
@@ -1,12 +1,17 @@
 import { Box, Flex, HStack, Text } from '@chakra-ui/react'
 import { Feed } from 'feed'
 import fs from 'fs'
+import rehypeStringify from 'rehype-stringify'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import { unified } from 'unified'
 import { allNows, allThoughts } from 'contentlayer/generated'
 
 import { Link } from '../components/base'
 import { SiteLayout } from '../components/layouts'
 import { PageWrapper } from '../components/site'
 import { PageHeading, SectionHeading } from '../components/typography'
+import remarkGfm from 'remark-gfm'
 
 const FeedsPage = () => {
   return (
@@ -61,28 +66,38 @@ export const getStaticProps = async () => {
 
   const items = [...allNows, ...allThoughts]
 
-  items
-    .sort((a, b) => {
-      if (new Date(a.date).getTime() > new Date(b.date).getTime()) {
-        return -1
-      }
+  await Promise.all(
+    items
+      .sort((a, b) => {
+        if (new Date(a.date).getTime() > new Date(b.date).getTime()) {
+          return -1
+        }
 
-      if (new Date(a.date).getTime() < new Date(b.date).getTime()) {
-        return 1
-      }
+        if (new Date(a.date).getTime() < new Date(b.date).getTime()) {
+          return 1
+        }
 
-      return 0
-    })
-    .map((item) => {
-      feed.addItem({
-        title: item.title,
-        id: `${SITE_URL}${item.url}`,
-        link: `${SITE_URL}${item.url}`,
-        description: item.description,
-        content: item.body.raw,
-        date: new Date(item.date),
+        return 0
       })
-    })
+      .map(async (item) => {
+        const content = await unified()
+          .use(remarkParse)
+          .use(remarkRehype)
+          .use(rehypeStringify)
+          .process(item.body.raw)
+
+        feed.addItem({
+          title: item.title,
+          id: `${SITE_URL}${item.url}`,
+          link: `${SITE_URL}${item.url}`,
+          description: item.description,
+          content: content.toString(),
+          date: new Date(item.date),
+        })
+
+        return Promise.resolve()
+      }),
+  )
 
   fs.writeFileSync('./public/feeds/all/rss.xml', feed.rss2())
   fs.writeFileSync('./public/feeds/all/rss.json', feed.json1())
