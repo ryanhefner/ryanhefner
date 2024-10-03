c8e4cdba196df0e2b90ebb1be9a6103013156eeb
Ryan Hefner <hi@ryanhefner.com>
2024-10-03 11:30:01 -0400

    Update blue

--- apps/site/src/styles/theme.ts
+++ apps/site/src/styles/theme.ts
@@ -22,7 +22,7 @@ export const theme = extendTheme(
     },
     colors: {
       blue: {
-        [500]: 'oklch(50% 0.2731 262.59)',
+        [500]: 'oklch(53.12% 0.2731 262.59)',
       },
       gray: {
         [500]: '#888',
