56682b0b2df34ac5b5bc6e7f4db947e425339544
Ryan Hefner <hi@ryanhefner.com>
2025-01-24 20:05:37 -0500

    Add YouTube link to header

--- apps/allplay/src/components/site/SiteHeader.tsx
+++ apps/allplay/src/components/site/SiteHeader.tsx
@@ -3,26 +3,47 @@ import { Flex, Heading, Text } from '@chakra-ui/react'
 import { Link } from '../base'
 
 export const SiteHeader = () => (
-  <Flex
-    as="header"
-    gap={8}
-    alignItems="center"
-    mixBlendMode="difference"
-    pos="sticky"
-    px={{ base: 4, md: 8 }}
-    pt={{ base: 6, md: 12 }}
-    top={0}
-    w="100%"
-    zIndex={1}
-  >
-    <Link href="/">
-      <Heading fontSize="lg">— All Play</Heading>
-    </Link>
-    <Link href="/podcast">
-      <Text fontSize="lg">Podcast</Text>
-    </Link>
-    <Link href="/newsletter">
-      <Text fontSize="lg">Newsletter</Text>
-    </Link>
-  </Flex>
+  <>
+    <Flex
+      bgColor="red.500"
+      color="white"
+      justifyContent="center"
+      py={2}
+      pos="fixed"
+      top={0}
+      left={0}
+      right={0}
+      zIndex={2}
+    >
+      <Link
+        href="https://youtube.com/playlist?list=PLuFdIOfmEGP0xvjt2yng9FErsMTVTk1Of&si=H0RpnIlvOhwg5Q2j"
+        isExternal
+      >
+        <Text>Watch the latest episodes on YouTube</Text>
+      </Link>
+    </Flex>
+    <Flex
+      as="header"
+      flexDir="column"
+      mixBlendMode="difference"
+      pos="sticky"
+      px={{ base: 4, md: 8 }}
+      pt={{ base: 16 }}
+      top={0}
+      w="100%"
+      zIndex={1}
+    >
+      <Flex alignItems="center" gap={8}>
+        <Link href="/">
+          <Heading fontSize="lg">— All Play</Heading>
+        </Link>
+        <Link href="/podcast">
+          <Text fontSize="lg">Podcast</Text>
+        </Link>
+        <Link href="/newsletter">
+          <Text fontSize="lg">Newsletter</Text>
+        </Link>
+      </Flex>
+    </Flex>
+  </>
 )
