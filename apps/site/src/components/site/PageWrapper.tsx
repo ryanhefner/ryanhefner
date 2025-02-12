bf7d992a33eb687d2795f3a3a2d12f6ef2c3a1e2
Ryan Hefner <hi@ryanhefner.com>
2025-02-11 23:12:21 -0500

    Add NewsletterForm to PageWrapper

--- apps/site/src/components/site/PageWrapper.tsx
+++ apps/site/src/components/site/PageWrapper.tsx
@@ -1,13 +1,36 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Heading, Text, useColorModeValue } from '@chakra-ui/react'
+import { NewsletterForm } from 'newsletter'
 
-export const PageWrapper = ({ children, ...rest }) => (
-  <Box
-    flex="1"
-    py={{ base: 6 }}
-    px={{ base: 6, sm: 10, md: 16, xl: 24 }}
-    w="full"
-    {...rest}
-  >
-    {children}
-  </Box>
-)
+export const PageWrapper = ({ children, ...rest }) => {
+  const bgColor = useColorModeValue('gray.50', 'gray.900')
+  const formVariant = useColorModeValue('light', 'dark')
+  return (
+    <>
+      <Box
+        flex="1"
+        py={{ base: 6, sm: 10, md: 16, xl: 24 }}
+        px={{ base: 6, sm: 10, md: 16, xl: 24 }}
+        mb={{ base: 16, md: 24 }}
+        w="full"
+        {...rest}
+      >
+        {children}
+      </Box>
+      <Box
+        id="signup"
+        bg={bgColor}
+        py={{ base: 12, md: 16, xl: 24 }}
+        px={{ base: 6, sm: 10, md: 16, xl: 24 }}
+        w="full"
+        pos="relative"
+      >
+        <Heading as="h3">Subscribe to the —All Play newsletter</Heading>
+        <Text>
+          Get updates when I publish new episodes, and post about other fun
+          stuff that I’m into.
+        </Text>
+        <NewsletterForm variant={formVariant} />
+      </Box>
+    </>
+  )
+}
