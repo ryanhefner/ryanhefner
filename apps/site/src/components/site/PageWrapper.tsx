2cd4544d82c482c5327454e0868d1e28caf1c9d6
Ryan Hefner <hi@ryanhefner.com>
2025-02-11 23:24:53 -0500

    Update PageWrapper styles

--- apps/site/src/components/site/PageWrapper.tsx
+++ apps/site/src/components/site/PageWrapper.tsx
@@ -2,15 +2,16 @@ import { Box, Heading, Text, useColorModeValue } from '@chakra-ui/react'
 import { NewsletterForm } from 'newsletter'
 
 export const PageWrapper = ({ children, ...rest }) => {
-  const bgColor = useColorModeValue('gray.50', 'gray.900')
+  const bgColor = useColorModeValue('gray.100', 'gray.900')
   const formVariant = useColorModeValue('light', 'dark')
+  const textColor = useColorModeValue('gray.500', 'gray.400')
+
   return (
     <>
       <Box
         flex="1"
         py={{ base: 6, sm: 10, md: 16, xl: 24 }}
         px={{ base: 6, sm: 10, md: 16, xl: 24 }}
-        mb={{ base: 16, md: 24 }}
         w="full"
         {...rest}
       >
@@ -25,7 +26,7 @@ export const PageWrapper = ({ children, ...rest }) => {
         pos="relative"
       >
         <Heading as="h3">Subscribe to the —All Play newsletter</Heading>
-        <Text>
+        <Text color={textColor}>
           Get updates when I publish new episodes, and post about other fun
           stuff that I’m into.
         </Text>
