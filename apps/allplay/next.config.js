735a593b55f1f206a8d2fd421e26d33cd38ab174
Ryan Hefner <hi@ryanhefner.com>
2025-10-21 13:48:58 -0400

    Enable next viewTransitions in config

--- apps/allplay/next.config.js
+++ apps/allplay/next.config.js
@@ -9,6 +9,7 @@ const { composePlugins, withNx } = require('@nx/next')
 const nextConfig = {
   experimental: {
     reactCompiler: true,
+    viewTransitions: true,
   },
   nx: {
     // Set this to true if you would like to to use SVGR
