9275fc084c0dc190ad230c51b2e2565f1a0e839e
Ryan Hefner <hi@ryanhefner.com>
2026-05-08 16:27:37 -0400

    Update NewsletterForm submit button

--- libs/newsletter/src/lib/components/NewsletterForm.tsx
+++ libs/newsletter/src/lib/components/NewsletterForm.tsx
@@ -130,17 +130,18 @@ export const NewsletterForm = ({
           <Box mt={{ base: 0, md: 7 }} w={{ base: 'full', md: 'auto' }}>
             <Button
               type="submit"
-              bg="red.500"
-              color="white"
+              bg="white"
+              color="black"
               flex="0 0 auto"
-              borderRadius="sm"
+              borderRadius="full"
               loading={isSubmitting}
               disabled={isSubmitting}
               aria-busy={isSubmitting}
               _hover={{ bg: 'red.400' }}
+              minW="120px"
               w={{ base: 'full', md: 'auto' }}
             >
-              {isSubmitting ? 'Subscribing...' : 'Subscribe'}
+              &rarr;
             </Button>
           </Box>
         </Flex>
